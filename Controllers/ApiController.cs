cebb98a9b33ba4df87d171b5af2fbde61eab808f
xexubonete <xexubonete@gmail.com>
2024-03-09 12:14:08 +0100

    Creating first two controllers

---
+++ Controllers/ApiController.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.CompilerServices;
+
+namespace dapper_api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public abstract class ApiController : ControllerBase
+    {
+        private IMediator? _mediator;
+
+        protected IMediator? mediator => _mediator;
+    }
+}
---
+++ Controllers/ClientController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace dapper_api.Controllers
+{
+    public class ClientController : ApiController
+    {
+        // GET: api/<ClientController>
+        [HttpGet]
+        public IEnumerable<string> GetAllClients()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<ClientController>/5
+        [HttpGet("{id}")]
+        public string GetClientById(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<ClientController>
+        [HttpPost]
+        public void CreateClient([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ClientController>/5
+        [HttpPut("{id}")]
+        public void UpdateClientById(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ClientController>/5
+        [HttpDelete("{id}")]
+        public void DeleteClientById(int id)
+        {
+        }
+    }
+}
