f63a71bd3ed91cbceac4663e511b623cdbc0a5ac
xexubonete <xexubonete@gmail.com>
2024-03-09 18:04:21 +0100

    Doing some work to show Lucia

---
+++ Services/Queries/GetAllClientsQuery.cs
@@ -0,0 +1,16 @@
+﻿using dapper_api.Entities;
+using MediatR;
+
+namespace dapper_api.Services.Queries
+{
+    public class GetAllClientsQuery : IRequest<List<Client>>
+    {
+        public class GetAllClientsQueryHandler : IRequestHandler<GetAllClientsQuery, IList<Client>>
+        {
+            public Task<IList<Client>> Handle(GetAllClientsQuery request, CancellationToken cancellationToken)
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
