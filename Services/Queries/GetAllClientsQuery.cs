1b0dfb2d19eb048e0d383a0ecd83b32608852e31
xexubonete <xexubonete@gmail.com>
2024-03-10 18:27:08 +0100

    Keeping working

--- Services/Queries/GetAllClientsQuery.cs
+++ Services/Queries/GetAllClientsQuery.cs
@@ -1,14 +1,19 @@
 ﻿using dapper_api.Entities;
+using dapper_api.Interfaces;
 using MediatR;
 
 namespace dapper_api.Services.Queries
 {
-    public class GetAllClientsQuery : IRequest<List<Client>>
+    public class GetAllClientsQuery : IRequest<IList<Client>>
     {
         public class GetAllClientsQueryHandler : IRequestHandler<GetAllClientsQuery, IList<Client>>
         {
+            private readonly IApiDbContext _context;
+
             public Task<IList<Client>> Handle(GetAllClientsQuery request, CancellationToken cancellationToken)
             {
+                var connectionString = _context.CreateConnection();
+
                 throw new NotImplementedException();
             }
         }
