644e3e768cb7218ba56a9b7982edee4ecc832018
xexubonete <xexubonete@gmail.com>
2024-03-10 18:26:36 +0100

    Creating DbContext class

---
+++ Interfaces/IApiDbContext.cs
@@ -0,0 +1,10 @@
+﻿using System.Data;
+using System.Data.Common;
+
+namespace dapper_api.Interfaces
+{
+    public interface IApiDbContext
+    {
+        public IDbConnection CreateConnection();
+    }
+}
---
+++ Models/ApiDbContext.cs
@@ -0,0 +1,15 @@
+﻿using dapper_api.Interfaces;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace dapper_api.Models
+{
+    public class ApiDbContext : IApiDbContext
+    {
+        public IDbConnection CreateConnection() 
+        {
+            string conectionString = "Server=(LocalDb)\\MSSQLLocalDB;Database=CrudApi;TrustServerCertificate=True;Integrated Security=True;";
+            return new SqlConnection(conectionString);
+        }
+    }
+}
