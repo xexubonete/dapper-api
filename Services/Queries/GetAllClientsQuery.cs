31e79128e944be3cdfca8daf6e2fa789f7364852
xexubonete <xexubonete@gmail.com>
2024-03-15 17:33:29 +0100

    Adding ctor

--- Services/Queries/GetAllClientsQuery.cs
+++ Services/Queries/GetAllClientsQuery.cs
@@ -9,11 +9,15 @@ namespace dapper_api.Services.Queries
     {
         public class GetAllClientsQueryHandler : IRequestHandler<GetAllClientsQuery, IEnumerable<Client>>
         {
-            private readonly IApiDbContext? _context;
+            private IApiDbContext _context;
+            public GetAllClientsQueryHandler(IApiDbContext context)
+            {
+                _context = context;
+            }
 
             public async Task<IEnumerable<Client>> Handle(GetAllClientsQuery request, CancellationToken cancellationToken)
             {
-                var connection = _context?.CreateConnection();
+                var connection = _context.CreateConnection();
 
                 string query = "SELECT * FROM CLIENT";
                 IEnumerable<Client>? result = await connection.QueryAsync<Client>(query);
