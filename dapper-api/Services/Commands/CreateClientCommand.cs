466ecabe45b1185403b36c128039e9e81ec4f73e
xexubonete <xexubonete@gmail.com>
2024-03-18 00:38:04 +0100

    Making the bussiness logic of CreateClientCommand

---
+++ dapper-api/Services/Commands/CreateClientCommand.cs
@@ -0,0 +1,40 @@
+﻿using Dapper;
+using dapper_api.Entities;
+using dapper_api.Interfaces;
+using MediatR;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dapper_api.Services.Commands
+{
+    public class CreateClientCommand : IRequest<Client>
+    {
+        public int? Id;
+        public string? Name;
+        public string? Surname;
+        public CreateClientCommand(Client? client)
+        {
+            Id = client.Id;
+            Name = client.Name;
+            Surname = client.Surname;
+        }
+
+        public class CreateClientCommandHandler : IRequestHandler<CreateClientCommand, Client>
+        {
+            private readonly IApiDbContext _context;
+            public CreateClientCommandHandler(IApiDbContext context)
+            {
+                _context = context;
+            }
+
+            public async Task<Client> Handle(CreateClientCommand request, CancellationToken cancellationToken)
+            {
+                var connection = _context.CreateConnection();
+                string query = $"INSERT INTO [dbo].[Client] ([Id], [Name], [Surname]) VALUES ({request.Id}, \'{request.Name}\', \'{request.Surname}\');";
+                var result = await connection.QueryAsync(query);
+
+                return result.FirstOrDefault();
+            }
+        }
+    }
+}
