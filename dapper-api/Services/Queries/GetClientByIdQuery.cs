bb0fea48272b6d3325af377e31fd95761521d675
xexubonete <xexubonete@gmail.com>
2024-03-17 23:53:28 +0100

    Creating de bussiness of dapper

---
+++ dapper-api/Services/Queries/GetClientByIdQuery.cs
@@ -0,0 +1,38 @@
+﻿using Dapper;
+using dapper_api.Entities;
+using dapper_api.Interfaces;
+using MediatR;
+using Microsoft.AspNetCore.Server.HttpSys;
+
+namespace dapper_api.Services.Queries
+{
+    public class GetClientByIdQuery : IRequest<Client>
+    {
+        public int Id;
+
+        public GetClientByIdQuery(int id)
+        {
+            Id = id;
+        }
+
+        public class GetClientByIdQueryHandler : IRequestHandler<GetClientByIdQuery, Client>
+        {
+
+            private readonly IApiDbContext _context;
+            public GetClientByIdQueryHandler(IApiDbContext context)
+            {
+                _context = context;
+            }
+
+            public async Task<Client> Handle(GetClientByIdQuery request, CancellationToken cancellationToken)
+            {
+                var connection = _context.CreateConnection();
+
+                string query = $"SELECT * FROM CLIENT WHERE ID = \'{request.Id}\' ";
+                var result = await connection.QueryAsync<Client>(query);
+
+                return result.FirstOrDefault();
+            }        
+        }
+    }
+}
