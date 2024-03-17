337961f49c567e17443a7883a978e8baf4e863ab
xexubonete <xexubonete@gmail.com>
2024-03-18 00:36:39 +0100

    Making mediator not nullable

--- dapper-api/Controllers/ApiController.cs
+++ dapper-api/Controllers/ApiController.cs
@@ -9,8 +9,8 @@ namespace dapper_api.Controllers
     [Route("api/[controller]")]
     public abstract class ApiController : ControllerBase
     {
-        private IMediator? _mediator;
+        private IMediator _mediator;
 
-        protected IMediator? Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
+        protected IMediator Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
     }
 }
