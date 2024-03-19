00f6776e02c0a765536ba9fdcaf34581f3f6d4a0
xexubonete <xexubonete@gmail.com>
2024-03-19 19:19:55 +0100

    Creating logic of DeleteClientByIdCommand

---
+++ dapper-api/Services/Commands/DeleteClientByIdCommand.cs
@@ -0,0 +1,36 @@
+﻿using Dapper;
+using dapper_api.Entities;
+using dapper_api.Interfaces;
+using MediatR;
+
+namespace dapper_api.Services.Commands
+{
+    public class DeleteClientByIdCommand : IRequest<Client>
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Surname { get; set; }
+        public DeleteClientByIdCommand(int id)
+        {
+            Id = id;
+        }
+
+        public class DeleteClientByIdCommandHandler : IRequestHandler<DeleteClientByIdCommand, Client>
+        {
+            private readonly IApiDbContext _context;
+            public DeleteClientByIdCommandHandler(IApiDbContext context)
+            {
+                _context = context;
+            }
+
+            public async Task<Client> Handle(DeleteClientByIdCommand request, CancellationToken cancellationToken)
+            {
+                var connection = _context.CreateConnection();
+                string command = $"DELETE FROM [dbo].[Client] WHERE [Id] = {request.Id}";
+                var result = (await connection.QueryAsync(command)).First();
+
+                return result;
+            }
+        }
+    }
+}
