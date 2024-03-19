75a3b24d69180321a54adaa0831a6130fefeded4
xexubonete <xexubonete@gmail.com>
2024-03-19 14:12:52 +0100

    Resolving a warning

--- dapper-api/Controllers/ApiController.cs
+++ dapper-api/Controllers/ApiController.cs
@@ -1,7 +1,5 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Mvc;
-using System.Reflection.Metadata.Ecma335;
-using System.Runtime.CompilerServices;
 
 namespace dapper_api.Controllers
 {
@@ -9,8 +7,14 @@ namespace dapper_api.Controllers
     [Route("api/[controller]")]
     public abstract class ApiController : ControllerBase
     {
+#pragma warning disable CS8618 // Un campo que no acepta valores NULL debe contener un valor distinto de NULL al salir del constructor. Considere la posibilidad de declararlo como que admite un valor NULL.
         private IMediator _mediator;
+#pragma warning restore CS8618 // Un campo que no acepta valores NULL debe contener un valor distinto de NULL al salir del constructor. Considere la posibilidad de declararlo como que admite un valor NULL.
 
+#pragma warning disable CS8603 // Posible tipo de valor devuelto de referencia nulo
+#pragma warning disable CS8601 // Posible asignación de referencia nula
         protected IMediator Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
+#pragma warning restore CS8601 // Posible asignación de referencia nula
+#pragma warning restore CS8603 // Posible tipo de valor devuelto de referencia nulo
     }
 }
