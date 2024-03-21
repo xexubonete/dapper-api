8ddf7b46bf9d2a6bedae514fa05ba022a701628a
xexubonete <xexubonete@gmail.com>
2024-03-21 17:09:20 +0100

    Adding new test project

---
+++ dapper-api-test/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace dapper_api_test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
