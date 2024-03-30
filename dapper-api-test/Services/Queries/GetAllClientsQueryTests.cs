513eb675b5e71bc41c5fd14c1b969cd99ec144b3
xexubonete <xexubonete@gmail.com>
2024-03-30 17:49:18 +0100

    Making GetAllClientsQuery test

--- dapper-api-test/Services/Queries/GetAllClientsQueryTests.cs
+++ dapper-api-test/Services/Queries/GetAllClientsQueryTests.cs
@@ -1,15 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using dapper_api.Models;
+using dapper_api.Services.Queries;
 
 namespace dapper_api_test.Services.Queries
 {
     public class GetAllClientsQueryTest
     {
+        private GetAllClientsQuery getAllClientsQuery;
+        private ApiDbContext _context;
+
         public GetAllClientsQueryTest()
         {
+            getAllClientsQuery = new GetAllClientsQuery();
+            _context = new ApiDbContext();
+        }
+
+        [Test]
+        public async Task GetAllClientsQuery_Should_Return_Records()
+        {
+            var handler =  new GetAllClientsQuery.GetAllClientsQueryHandler(_context);
+            var result = await handler.Handle(getAllClientsQuery, CancellationToken.None);
+
+            Assert.That(result, Is.Not.Null);
         }
     }
 }
