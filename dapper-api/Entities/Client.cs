1a1fb16d0fca2c126819447e5729dcbd546b1758
xexubonete <xexubonete@gmail.com>
2024-08-16 00:51:04 +0200

    [FEAT] Added correct responses for new uses cases

--- dapper-api/Entities/Client.cs
+++ dapper-api/Entities/Client.cs
@@ -5,5 +5,17 @@
         public int Id { get; set; }
         public string? Name { get; set; }
         public string? Surname { get; set; }
+#pragma warning disable CS8765 // La nulabilidad del tipo de parámetro no coincide con el miembro invalidado (posiblemente debido a los atributos de nulabilidad).
+        public override bool Equals(object obj)
+#pragma warning restore CS8765 // La nulabilidad del tipo de parámetro no coincide con el miembro invalidado (posiblemente debido a los atributos de nulabilidad).
+        {
+            if (obj is Client other)
+            {
+                return Id == other.Id &&
+                    Name == other.Name &&
+                    Surname == other.Surname;
+            }
+            return false;
+        }
     }
 }
