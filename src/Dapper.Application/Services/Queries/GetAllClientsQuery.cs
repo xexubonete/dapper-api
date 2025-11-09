a22c37411ea454cd175de06c74e38ce956f0fce2
xexu <xexubonete@gmail.com>
2025-11-09 12:46:20 +0100

    Added hardcode list of clients. Delete String.Utils dependency. Refactoring setter of Id ClientDTO to init.

--- src/Dapper.Application/Services/Queries/GetAllClientsQuery.cs
+++ src/Dapper.Application/Services/Queries/GetAllClientsQuery.cs
@@ -16,8 +16,23 @@ namespace Dapper.Application.Services.Queries
                     using var connection = context.CreateConnection();
 
                     string query = $"SELECT * FROM Client";
-
-                    return await connection.QueryAsync<ClientDto?>(query);
+                    
+                    List<ClientDto> clients = new()
+                    {
+                        new ClientDto { Id = 1, Name = "Laura", Surname = "Martínez" },
+                        new ClientDto { Id = 2, Name = "Carlos", Surname = "García" },
+                        new ClientDto { Id = 3, Name = "Ana", Surname = "López" },
+                        new ClientDto { Id = 4, Name = "Javier", Surname = "Sánchez" },
+                        new ClientDto { Id = 5, Name = "Lucía", Surname = "Pérez" },
+                        new ClientDto { Id = 6, Name = "Andrés", Surname = "Fernández" },
+                        new ClientDto { Id = 7, Name = "Marta", Surname = "Ruiz" },
+                        new ClientDto { Id = 8, Name = "Pablo", Surname = "Torres" },
+                        new ClientDto { Id = 9, Name = "Sofía", Surname = "Gómez" },
+                        new ClientDto { Id = 10, Name = "Hugo", Surname = "Navarro" }
+                    };
+                    // return await connection.QueryAsync<ClientDto?>(query);
+                    
+                    return clients; 
                 }
                 catch (Exception ex)
                 {
--- src/Dapper.Domain/Dtos/ClientDto.cs
+++ src/Dapper.Domain/Dtos/ClientDto.cs
@@ -2,7 +2,7 @@ namespace Dapper.Domain.Dtos
 {
     public record ClientDto
     {
-        public int Id { get; }
+        public int Id { get; init; }
         public string? Name { get; set; }
         public string? Surname { get; set; }
     }   
